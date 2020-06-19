9ef32157b372b5b1ad4d620917bdf31c408ba27b
adamrpostjr <adamrpostjr@gmail.com>
2020-06-18 21:29:42 -0400

    started setting up login screen

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require("express");
-
-
 const younameitvpn = express();
 
 
 younameitvpn.use(express.static('public'));
-younameitvpn.use('/pages', express.static(__dirname + '/public/pages'));
-younameitvpn.use('/css', express.static(__dirname + '/public/css'));
-younameitvpn.use('/js', express.static(__dirname + '/public/js'));
-younameitvpn.use('/images', express.static(__dirname + '/public/images'));
+
 
 const server = younameitvpn.listen(8080, function(){
     var port = server.address().port;
@@ -16,15 +11,15 @@ const server = younameitvpn.listen(8080, function(){
 });
 const login = __dirname + '/public/pages/index.html'
 const dashboard = __dirname + '/public/pages/dashboard.html'
-
+const admin = __dirname + '/public/pages/admin.html'
 
 
 // login
 younameitvpn.get("/", (req, res) => {
-  res.render(login)
+  res.sendFile(login)
 });
 // Dashboard
 younameitvpn.get("/dashboard", (req, res) => {
   // res.render(dashboard, { freeMem : fmem });
-  res.render(dashboard);
+  res.sendFile(dashboard);
 });
