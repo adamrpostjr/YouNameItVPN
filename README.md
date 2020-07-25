6594090649e94c4596807efcc3e1903d7dc731a4
Adam <adamrpostjr@gmail.com>
2020-07-25 15:56:10 -0400

    Getting things ready to start testing

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # YouNameItVPN
 A PostalProduction featuring NodeJS and Wireguard
 
+This is a WireGuard Web interface that will allow you to setup a personalized Site2Site / For Sale VPN
 
-This is a Wireguard Configuration / Web interface / User Registration System.
-
-# Instructions
-~~ To Come ~~
+# Instruction
+## Prerequisite ##
+-- This will not always be like this -- I want to setup a deployment script per distro
+1. Install WireGuard -- No config needed
+2. Install MySQL-Server -- Run through 'mysql_secure_installation utility'
+3. Create MySQL user for the application
+4. Clone this to where ever -- I like /var/www/YouNameItVPN
 
 # ToDo
-Setup SSL
+1. Get everything working as per my idea file
+2. Deployment Script
+
+
+#Ideas
+located in the idea file I'll move over to here when I feel like it.
--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const dotenv = require('dotenv');
+const mysql = require('mysql');
+const fs = require('fs');
+const ejs = require('ejs');
+
 const younameitvpn = express();
 
+younameitvpn.set('view-engine', 'ejs');
 younameitvpn.use(express.static('public'));
 const server = younameitvpn.listen(8080, function(){
     var port = server.address().port;
     console.log("Server started at http://192.168.0.43:%s", port);
     console.log("Server started at http://24.55.146.24:%s", port);
 });
-// Pages that will need to be loaded
 
-const login = __dirname + '/public/pages/index.html'
-const dashboard = __dirname + '/public/pages/dashboard.html'
-const admin = __dirname + '/public/pages/admin.html'
+//Location
+const setup = __dirname + '/Setup/Pages/setupindex.ejs'
+
+//env file Location
+const env = "./.env"
+
+try {
+  if (fs.existsSync(env)) {
+    //env exit.. we need to do some checks on then pick up where we left off
+  }else {
+    //this will be offloaded onto the ./Setup/setup.js
+  }
+} catch (e) {
+  console.log(e);
+}
